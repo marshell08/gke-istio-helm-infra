70316c4651c8b8e185f8e61bb335a9f2cb395980
marshell <marshell@google.com>
2019-04-22 19:49:33 -0400

    Change istio version and create namespace

--- install_istio.sh
+++ install_istio.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ISTIO_VERSION=1.1.3
+export ISTIO_VERSION=1.0.0
 
 echo "download istio"
 curl -L https://git.io/getLatestIstio | ISTIO_VERSION=${ISTIO_VERSION} sh -
@@ -10,6 +10,9 @@ cd istio-${ISTIO_VERSION}/
 
 echo "add istio to the path"
 export PATH=${PWD}/bin:$PATH
+
+echo "Create namespace istio-system"
+kubectl create ns istio-system
 
 echo "install istio - this might take a moment..."
 helm install install/kubernetes/helm/istio \
