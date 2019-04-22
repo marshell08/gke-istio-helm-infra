aff4b6f609ab51ccd8a7bed9af33fffd5d4a2f78
Marshell Rodrigues <marshell@google.com>
2019-04-22 19:37:37 -0400

    Update install_helm.sh

--- install_helm.sh
+++ install_helm.sh
@@ -1,91 +1,26 @@
 #!/usr/bin/env bash
 
 echo "install helm"
+
+# installs helm with bash commands for easier command line integration
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 
-# kubectl --namespace kube-system create sa tiller
-kubectl create -f values/rbac-config.yaml
+# add a service account within a namespace to segregate tiller
+kubectl --namespace kube-system create sa tiller
 
-echo "create tiller namespace"
-kubectl create namespace tiller
-kubectl create serviceaccount tiller --namespace tiller
-kubectl create -f values/role-tiller.yaml
-kubectl create -f values/rolebinding-tiller.yaml
-
-echo "preclean"
-rm ca.* tiller.* helm.*
-
-echo "set key values"
-export SUBJECT="/C=US/ST=SanFrancisco/L=Californis/O=Noname ltd/OU=GKE/CN=example.com"
-
-echo "create certs"
-openssl genrsa -out ca.key.pem 4096
-openssl req -key ca.key.pem -new -x509 \
-    -days 7300 -sha256 \
-    -out ca.cert.pem \
-    -extensions v3_ca \
-    -subj "${SUBJECT}"
-# one per tiller host
-openssl genrsa -out tiller.key.pem 4096
-# one PER user (in this case helm is the user)
-openssl genrsa -out helm.key.pem 4096
-# create certificates for each of the keys
-openssl req \
-    -key tiller.key.pem \
-    -new \
-    -sha256 \
-    -out tiller.csr.pem \
-    -subj "${SUBJECT}"
-openssl req \
-    -key helm.key.pem \
-    -new \
-    -sha256 \
-    -out helm.csr.pem \
-    -subj "${SUBJECT}"
-# sign each of the CSRs with the CA cert
-openssl x509 -req \
-    -CA ca.cert.pem \
-    -CAkey ca.key.pem \
-    -CAcreateserial \
-    -in tiller.csr.pem \
-    -out tiller.cert.pem \
-    -days 365
-openssl x509 -req \
-    -CA ca.cert.pem \
-    -CAkey ca.key.pem \
-    -CAcreateserial \
-    -in helm.csr.pem \
-    -out helm.cert.pem \
-    -days 365
+# create a cluster role binding for tiller
+kubectl create clusterrolebinding tiller \
+    --clusterrole cluster-admin \
+    --serviceaccount=kube-system:tiller
 
 echo "initialize helm"
-helm init \
-    --tiller-tls \
-    --tiller-tls-cert tiller.cert.pem \
-    --tiller-tls-key tiller.key.pem \
-    --tiller-tls-verify \
-    --tls-ca-cert ca.cert.pem \
-    --service-account tiller
+# initialized helm within the tiller service account
+helm init --service-account tiller
+
+# updates the repos for Helm repo integration
 helm repo update
-sleep 5
 
 echo "verify helm"
+
+# verify that helm is installed in the cluster
 kubectl get deploy,svc tiller-deploy -n kube-system
-helm ls \
-    --tls \
-    --tls-ca-cert ca.cert.pem \
-    --tls-cert helm.cert.pem \
-    --tls-key helm.key.pem
-
-echo "move certs"
-# you move them so you don't need to include them with every call to helm
-cp ca.cert.pem $(helm home)/ca.pem
-cp helm.cert.pem $(helm home)/cert.pem
-cp helm.key.pem $(helm home)/key.pem
-
-echo "verify security"
-sleep 3
-helm ls --tls
-
-echo "------------------------------------"
-echo "helm is now installed"
