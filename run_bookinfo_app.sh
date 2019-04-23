c68eeaa9907aed8e7f68e5eccf8782fd043518ca
Marshell Rodrigues <marshell@google.com>
2019-04-22 21:25:18 -0400

    Update run_bookinfo_app.sh

--- run_bookinfo_app.sh
+++ run_bookinfo_app.sh
@@ -3,13 +3,12 @@ export ISTIO_VERSION=1.0.0
 cd istio-${ISTIO_VERSION}/
 
 echo "launch bookinfo sample on gke"
-kubectl create ns bookinfo-dev
 
-kubectl label namespace bookinfo-dev istio-injection=enabled
+kubectl label namespace default istio-injection=enabled
 
-kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml  --namespace=bookinfo-dev
+kubectl apply -f samples/bookinfo/platform/kube/bookinfo.yaml  
 sleep 10
 
 echo "add in the ingress"
-kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml --namespace=bookinfo-dev
+kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml 
 kubectl get gateway
