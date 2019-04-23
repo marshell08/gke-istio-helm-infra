6231988510f898979d0bbc2e1bdb598d64effceb
Marshell Rodrigues <marshell@google.com>
2019-04-22 22:47:21 -0400

    Create install_hipster_app.sh

---
+++ microservices-cicd-demo-3/install_hipster_app.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
+cd microservices-demo
+
+kubectl label namespace default istio-injection=enabled
+kubectl apply -f ./istio-manifests
+
+#skaffold run --default-repo=gcr.io/[PROJECT_ID]
+skaffold run --default-repo=gcr.io/microservices-cicd-demo-3
+
