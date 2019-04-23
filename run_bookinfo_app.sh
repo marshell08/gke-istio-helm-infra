02df79a81d214f474def512c19fb794b921ec46d
Marshell Rodrigues <marshell@google.com>
2019-04-22 20:06:03 -0400

    Update run_bookinfo_app.sh

--- run_bookinfo_app.sh
+++ run_bookinfo_app.sh
@@ -13,13 +13,3 @@ sleep 10
 echo "add in the ingress"
 kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
 kubectl get gateway
-
-echo "checking for gateway completion"
-sleep 30
-sh samples/check-bookinfo-gateway.sh
-
-echo "if this errors out you can always retry by calling:"
-echo "sh samples/check-bookinfo-gateway.sh"
-
-echo "you'll also want to add in the version routing for the samples:"
-echo "sh samples/add-bookinfo-version-routing.sh"
