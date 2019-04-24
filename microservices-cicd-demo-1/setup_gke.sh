b06e4c0d144d3ec40be15868ccf44fde46fd59d6
Marshell Rodrigues <marshell@google.com>
2019-04-24 12:08:09 -0400

    Update setup_gke.sh

--- microservices-cicd-demo-1/setup_gke.sh
+++ microservices-cicd-demo-1/setup_gke.sh
@@ -6,8 +6,20 @@ export ZONE=us-central1-b
 
 gcloud config set project microservices-lcbo-demo
 
+gcloud services enable container.googleapis.com 
+gcloud services enable containerregistry.googleapis.com 
+gcloud services enable cloudbuild.googleapis.com 
+gcloud services enable sourcerepo.googleapis.com 
+
 gcloud container clusters create ${CLUSTER} \
     --project=${PROJECT} \
     --zone=${ZONE} \
-    --num-nodes=5
     --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
+
+
+export PROJECT_NUMBER="$(gcloud projects describe \
+    $(gcloud config get-value core/project -q) --format='get(projectNumber)')"
+
+gcloud projects add-iam-policy-binding ${PROJECT} \
+    --member=serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com \
+    --role=roles/container.developer
