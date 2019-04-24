e3656decd2bfd1fad25c454addf55422d0be8a2d
Marshell Rodrigues <marshell@google.com>
2019-04-24 11:11:02 -0400

    Create first_time_deploy.sh

---
+++ microservices-cicd-demo-1/first_time_deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+gcloud container clusters get-credentials gke-deploy-cluster --project=microservices-lcbo-demo --zone=us-central1-b
+
+kubectl create ns production
+
+kubectl apply -f kubernetes/deployments/prod -n production
+kubectl apply -f kubernetes/deployments/canary -n production
+kubectl apply -f kubernetes/services -n production
+
+
+kubectl get pods -n production -l app=gceme -l role=frontend
+kubectl get pods -n production -l app=gceme -l role=backend
+
+kubectl get service gceme-frontend -n production
+export FRONTEND_SERVICE_IP=$(kubectl get -o jsonpath="{.status.loadBalancer.ingress[0].ip}"  --namespace=production services gceme-frontend)
+
+curl http://$FRONTEND_SERVICE_IP/version
