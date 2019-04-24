809250037d21646fba4f35b95771e3a9e0d8c6d6
Marshell Rodrigues <marshell@google.com>
2019-04-24 12:57:15 -0400

    Update first_time_deploy.sh

--- microservices-cicd-demo-1/first_time_deploy.sh
+++ microservices-cicd-demo-1/first_time_deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gcloud container clusters get-credentials gke-deploy-cluster --project=microservices-lcbo-demo --zone=us-central1-b
+gcloud container clusters get-credentials gke-deploy-cluster --project=microservices-lcbo-final-demo1 --zone=us-central1-b
 git clone git@github.com:marshell08/gcp-go-microservice-app1.git -b master
 cd gcp-go-microservice-app1
 
@@ -16,6 +16,6 @@ kubectl get pods -n production -l app=gceme -l role=frontend
 kubectl get pods -n production -l app=gceme -l role=backend
 
 kubectl get service gceme-frontend -n production
-export FRONTEND_SERVICE_IP=$(kubectl get -o jsonpath="{.status.loadBalancer.ingress[0].ip}"  --namespace=production services gceme-frontend)
 
-curl http://$FRONTEND_SERVICE_IP/version
+#export FRONTEND_SERVICE_IP=$(kubectl get -o jsonpath="{.status.loadBalancer.ingress[0].ip}"  --namespace=production services gceme-frontend)
+#curl http://$FRONTEND_SERVICE_IP/version
